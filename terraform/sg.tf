b728aecef07fb6733c9160acfc3817a21df60cbd
Derek Berger <derekberger734@gmail.com>
2025-04-18 11:13:50 -0400

    Update security group

--- terraform/sg.tf
+++ terraform/sg.tf
@@ -7,35 +7,52 @@ resource "aws_security_group" "instance_sg" {
   vpc_id      = data.terraform_remote_state.vpc_workspace.outputs.vpc_id
 
   ingress = [{
+    cidr_blocks      = [data.terraform_remote_state.vpc_workspace.outputs.vpc_cidr_block]
     description      = "SSH from ${local.name_prefix}-sshaccess-sg"
     from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = [data.terraform_remote_state.vpc_workspace.outputs.vpc_cidr_block]
     ipv6_cidr_blocks = []
+    prefix_list_ids  = []
+    protocol         = "tcp"
+    security_groups = []
+    self    = false
+    to_port = 22
   }]
 
-  egress = [{
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = []
-  }]
+  egress = [
+    {
+      cidr_blocks = [
+        "0.0.0.0/0",
+      ]
+      description      = ""
+      from_port        = 0
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "-1"
+      security_groups  = []
+      self             = false
+      to_port          = 0
+    },
+  ]
 }
 
 resource "aws_security_group" "sshaccess_sg" {
   name        = "${local.name_prefix}-sshaccess-sg"
   description = "Allow SSH access to ${local.name_prefix} hosts"
   vpc_id      = data.terraform_remote_state.vpc_workspace.outputs.vpc_id
-
   ingress = []
-
-  egress = [{
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = []
-  }]
+  egress = [
+    {
+      cidr_blocks = [
+        "0.0.0.0/0",
+      ]
+      description      = ""
+      from_port        = 0
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "-1"
+      security_groups  = []
+      self             = false
+      to_port          = 0
+    }
+  ]
 }
