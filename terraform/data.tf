fec0e8985be03d4492f8e182ec5b526e50f66941
Derek Berger <derekberger734@gmail.com>
2025-04-20 19:12:29 -0400

    Update data lookups

--- terraform/data.tf
+++ terraform/data.tf
@@ -2,11 +2,7 @@
 # AWS Data Providers #
 ######################
 data "aws_region" "current" {}
-
 data "aws_caller_identity" "current" {}
-
-data "aws_iam_account_alias" "current" {}
-
 data "aws_partition" "current" {}
 
 ##################################
